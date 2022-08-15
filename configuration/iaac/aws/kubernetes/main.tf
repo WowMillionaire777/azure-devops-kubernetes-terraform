218db4017eeca4d56cb806269535040d6da14104
WowMillionaire777 <johnkenn777@outlook.com>
2022-08-15 16:09:44 -0400

    updated node group classification on  main.tf

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -40,7 +40,7 @@ module "in28minutes-cluster" {
   vpc_id          = vpc-0cbc98d2667f37c82
   #vpc_id         = "vpc-0cbc98d2667f37c82"
 
-  node_groups = [
+  self_managed_node_groups_defaults = [
     {
       instance_type = "t2.micro"
       max_capacity  = 5
