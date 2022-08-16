9ebd47e13a970994a128ec26c2a2d316cef01c82
WowMillionaire777 <johnkenn777@outlook.com>
2022-08-16 16:58:03 -0400

    updated and cleaned up tf files

--- configuration/iaac/aws/kubernetes/state.tf
+++ configuration/iaac/aws/kubernetes/state.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-state-in28minutes-123" # Will be overridden from build
-    key    = "kubernetes-dev.tfstate" # Will be overridden from build
-    region = "us-east-1"
+    bucket         = "terraform-backend-state-5678" # Will be overridden from build
+    key            = "kubernetes-dev.tfstate" # Will be overridden from build
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
   }
 }
